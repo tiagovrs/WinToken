352b24af09e69052950435cc090b9e4e835fab1c
Thiago Gomes <thiago.gomes@sou.inteli.edu.br>
2024-05-04 16:25:55 -0300

    Cria a tela de login.

--- src/pages/scan/Scan.jsx
+++ src/pages/scan/Scan.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Header from "../components/header/Header";
+import BottomNav from "../components/bottomNav/BottomNav";
+import { faBarcode } from "@fortawesome/free-solid-svg-icons";
+import "./scan.css"
+
 export default function Scan() {
-    return (
-      <div>
-        <h1>Scan</h1>
-      </div>
-    );
+  const [searchvalue, setSearchValue] = useState("");
+
+  function handleChange(event) {
+    setSearchValue(event.target.searchvalue);
   }
+
+  return (
+    <body>
+      <Header />
+      <div className="camScrennContainer">
+      <div className="camContainer"></div>
+      <div className="inputDescCamContainer">
+        <h2 className="qrTitle">Aponte para o QR code <br /> ou</h2>
+        <div className="qrSearch">
+          <input
+            type="text"
+            value={searchvalue}
+            onChange={handleChange}
+            placeholder="Digite o código..."
+          />
+          <FontAwesomeIcon className="qrIcon" icon={faBarcode} />
+        </div>
+      </div>
+      </div>
+      <BottomNav />
+    </body>
+  );
+}
