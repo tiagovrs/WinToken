e261b453c56752f297aff557a02212a1b2c2c7af
Vinicius Testa Passos <viniciustestapassos@gmail.com>
2024-05-05 17:35:53 -0300

    feat: adiciona o contrato de nft ao projeto

--- src/pages/roullete/Roullete.jsx
+++ src/pages/roullete/Roullete.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 import { Wheel } from "react-custom-roulette";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
 import BottomNav from "../components/bottomNav/BottomNav";
 import Header from "../components/header/Header";
 import "./roullete.css";
+import axios from "axios";
+import { apiKey } from "../../config/api/axios";
+import { winNft } from "../../contracts/WinNFT";
+import { ethers } from "ethers";
+import { provider } from "../../config/api/axios";
+import winTokenAbi, { erc20TokenAddress } from "../../contracts/WinTokenAbi";
 
 const data = [
   {
@@ -25,9 +32,21 @@ const data = [
   { option: "Grind Guru", style: { backgroundColor: "#4c4c4c" } },
 ];
 
+const override = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
+
+
 export default function Bets() {
+  const [loading, setLoading] = useState(true);
+  const [color, setColor] = useState("#ffffff");
+  const [mint, setMint] = useState(false);
   const [mustSpin, setMustSpin] = useState(false);
   const [confirmSpin, setConfirmSpin] = useState(false);
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+  const [rollEnabled, setRollEnabled] = useState(false);
 
   const handleSpinClick = () => {
     if (!mustSpin && !confirmSpin) {
@@ -38,12 +57,89 @@ export default function Bets() {
     }
   };
 
-  const handleConfirmSpin = (confirm) => {
-    if (confirm) {
-      setMustSpin(true);
-      setConfirmSpin(false);
-    } else {
-      setConfirmSpin(false);
+  const handleGetBalance = async () => {
+    const contract = new ethers.Contract(erc20TokenAddress, winTokenAbi, provider);
+    const wTks = ethers.formatEther(await contract.balanceOf(user.wallet)) * 10**18;
+    if (wTks >= 150) {
+      setRollEnabled(true);
+    }
+  };
+
+  handleGetBalance();
+
+  const mintNft = async (reedemData) => {
+    const response = await axios.post("https://protocol-sandbox.lumx.io/v2/transactions/custom", 
+    {
+      "contractAddress": winNft,
+      "operations": [
+        {
+          "functionSignature": "safeMint(address)",
+          "argumentsValues": [
+            user.wallet
+          ],
+          "messageValue": 0
+        }
+      ],
+      "walletId": user.walletId
+    },
+    {
+      headers: {
+        'Authorization': `Bearer ${apiKey}`
+      }
+    })
+    
+    return response.data;
+  };
+
+  const burnWtk = async (reedemData) => {
+    const response = await axios.post("https://protocol-sandbox.lumx.io/v2/transactions/custom", 
+    {
+      "contractAddress": erc20TokenAddress,
+      "operations": [
+        {
+          "functionSignature": "burn(uint256)",
+          "argumentsValues": [
+            150
+          ],
+          "messageValue": 0
+        }
+      ],
+      "walletId": user.walletId
+    },
+    {
+      headers: {
+        'Authorization': `Bearer ${apiKey}`
+      }
+    })
+    
+    return response.data;
+  };
+
+  const finishSpin = async () => {
+    if(confirm("Parabéns, você recebeu um NFT! Deseja visualizar o NFT?")) {
+      window.open(`https://testnets.opensea.io/${user.wallet}`)
+    }
+  }
+
+  const handleConfirmSpin = async (confirm) => {
+    try {
+      if (confirm) {
+        try {
+          setConfirmSpin(false);
+          setMint(true);
+          const response = await mintNft();
+          const burnResponse = await burnWtk();
+          
+          setMint(false);
+          setMustSpin(true);
+        } catch (error) {
+          console.error('Erro ao buscar dados:', error);
+        }
+      } else {
+        setConfirmSpin(false);
+      }
+    } catch (error) {
+      alert('Erro ao girar a roleta', error)
     }
   };
 
@@ -56,7 +152,19 @@ export default function Bets() {
           <p>Aposte seus WTKs e tenha a chance de conseguir NFTs raros</p>
         </div>
 
-        <div className={`roulleteContainer ${mustSpin ? "spinning" : ""}`}>
+        
+        <div style={{display: mint ? "flex" : "none", alignItems: "center", justifyContent: "center", height: "100%", width: "100%"}}>
+          <ClipLoader
+            color={color}
+            loading={loading}
+            cssOverride={override}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      
+        <div style={{display: !mint ? "flex" : "none"}} className={`roulleteContainer ${mustSpin ? "spinning" : ""}`}>
           <div className="roullete">
             <Wheel
               mustStartSpinning={mustSpin}
@@ -71,8 +179,8 @@ export default function Bets() {
               textDistance={55}
               spinDuration={0.75}
               textColors={["#eee"]}
-              onStopSpinning={() => {
-                console.log("Roleta girada");
+              onStopSpinning={async () => {
+                await finishSpin();
                 setMustSpin(false);
               }}
             />
@@ -80,7 +188,7 @@ export default function Bets() {
               <FontAwesomeIcon className="blockedRoulleteIcon" icon={faLock} />
             )}
           </div>
-          <button onClick={handleSpinClick} className="spinBtn">
+          <button onClick={handleSpinClick} disabled={!rollEnabled} style={{background: !rollEnabled ? "gray" : "#a25ca4" }} className="spinBtn">
             150 WTKs
           </button>
         </div>
