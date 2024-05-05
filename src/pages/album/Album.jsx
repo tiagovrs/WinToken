6e48afc009d372553df5ec01b1a9d66663d8b4b3
Thiago Gomes <thiago.gomes@sou.inteli.edu.br>
2024-05-05 02:16:21 -0300

    Adiciona tela Álbum.

--- src/pages/album/Album.jsx
+++ src/pages/album/Album.jsx
@@ -1,9 +1,38 @@
-import "./album.css"
+import Header from "../components/header/Header";
+import BottomNav from "../components/bottomNav/BottomNav";
+import NftCard from "./nftCard/NftCard";
+import "./album.css";
 
-export default function Album(){
-  return(
-    <div>
-      <h1>Álbum</h1>
-    </div>
-  )
+export default function Album() {
+  return (
+    <body>
+      <Header />
+      <div className="albumContainer">
+        <h2 className="albumTitle">Álbum de NFTs</h2>
+        <div className="cardsContainer">
+          <NftCard
+            urlImg="src\assets\albumNfts\TouchdownTitan.jpeg"
+            title="Touchdown Titan"
+            price={780}
+          />
+          <NftCard
+            urlImg="src\assets\albumNfts\GoalGlory.jpeg"
+            title="GoalGlory"
+            price={250}
+          />
+          <NftCard
+            urlImg="src\assets\albumNfts\DunkDynasty.jpeg"
+            title="Dunk Dynasty"
+            price={160}
+          />
+          <NftCard
+            urlImg="src\assets\albumNfts\HomeRunHero.jpeg"
+            title="Home Run Hero"
+            price={240}
+          />
+        </div>
+      </div>
+      <BottomNav />
+    </body>
+  );
 }
---
+++ src/pages/nftCard/Album.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/header/Header";
+import BottomNav from "../../components/bottomNav/BottomNav";
+import NftCard from "../components/nftCard/NftCard";
+import "./album.css"
+
+export default function Album(){
+  return(
+    <div>
+      <h1>Álbum</h1>
+    </div>
+  )
+}
